0c3639f19db231027e0cb935f6ecb7be0fffbd5e
Aiden <apalecz2@uwo.ca>
2025-05-31 12:24:26 -0400

    Added map boundaries, user location, and test markers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import Map from './components/Map';
 
 function App() {
-  
 
-  return (
-    <>
-        <Map />
-      <div>
-        <h1 className="text-2xl m-20">Title</h1>
-      </div>
-    </>
-  )
+
+    return (
+        <>
+            <Map />
+            {/*<div>
+                <h1 className="text-2xl m-20">Title</h1>
+            </div>*/}
+        </>
+    )
 }
 
 export default App
